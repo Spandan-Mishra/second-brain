8ef0e566b88bde6a825f3e7f0a5c4a95d88addb5
Spandan-Mishra <spandanmishra69@gmail.com>
2024-11-20 11:30:47 +0530

    Button comp added

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -2,11 +2,32 @@ export  interface ButtonProps {
     variant: "primary" | "secondary";
     size: "sm" | "md" | "lg";
     text: string;
-    startIcon: any;
-    endIcon: any;
+    startIcon?: any;
+    endIcon?: any;
     onClick: () => void;
 }
 
-export const Button = () => {
+const variantStyles = {
+    "primary": "bg-purple-600 text-white",
+    "secondary": "bg-purple-300 text-purple-600"
+}
+
+const defaultStyles = "rounded-md p-4"
+
+const sizeStyles = {
+    "sm": "py-1 px-2",
+    "md": "py-2 px-4",
+    "lg": "py-4 px-6"
+}
+
+export const Button = (props: ButtonProps) => {
+
+    return <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]}`}>
+        {props.text}
+    </button>
 
 }
+
+
+
+<Button variant="primary" size="md" text="Signin" onClick={() => {}} />
