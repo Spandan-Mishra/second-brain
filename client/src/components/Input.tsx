bc8fa98a7fa68b68668f37277f6f7b18d11ac2a8
Spandan-Mishra <spandanmishra69@gmail.com>
2024-12-21 16:24:55 +0530

    Validation messages format changed

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Input = ({ reference, placeholder }: { reference: HTMLInputElement, placeholder: string }) => {
   return <div>
-    <input placeholder={placeholder} type="text" className="px-4 py-2 border rounded m-2" ref={reference} />
+    <input placeholder={placeholder} type={placeholder === "Password" ? "password" : "text"} className="px-4 py-2 border rounded m-2" ref={reference} />
   </div>
 }
